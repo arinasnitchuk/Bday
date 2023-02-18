b5bda2a8677bef2156bfa495b44950065da9cd2e
leshaluffscats <alexanikeenko317@gmail.com>
2023-02-19 01:36:58 +0300

    3rd task

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,33 @@ const swiper = new Swiper('.swiper', {
         dynamicBullets: true,
     },
 });
+
+const images = document.querySelectorAll(".image-block img");
+const imageBlock = document.querySelectorAll(".image-block");
+const container22 = document.querySelector("#result");
+const startBtn = document.querySelector("#start");
+
+const arr = [];
+
+imageBlock.forEach(block => {
+    block.addEventListener("touchstart", () => {
+        arr.push(block);
+        block.remove();
+        renderResult();
+    });
+});
+
+function renderResult() {
+    if (arr.length === 0) {
+        return;
+    } else {
+        arr.forEach(el => container22.append(el));
+    }
+}
+
+
+startBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    location.href = "#task1";
+})
+
